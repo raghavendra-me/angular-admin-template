1eb91749a656a4435093b1037c29e1d544b1e3d8
raghavendra-me <iamraghavendra7566@gmail.com>
2024-09-26 12:37:38 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, RouterLink, HeaderComponent]
 })
 export class AppComponent {
-  title = 'angular-admin';
+  title = 'angularadmin';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SamplepageComponent } from './samplepage/samplepage.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', title: 'Dashboard Page', component: DashboardComponent,
+      },
+      {
+        path: 'samplepage', title: 'Sample Page', component: SamplepageComponent,
+      },
+];
